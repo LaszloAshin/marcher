797436c58d9f3af8268fc58ad425b8e8b781a0a3
Laszlo Ashin <laszlo@ashin.hu>
2011-11-01 21:08:06 +0100

    water, sky, etc

--- marcher.cc
+++ marcher.cc
@@ -75,6 +75,7 @@ public:
 	Pixel &max(const Pixel &p);
 
 	Pixel operator*(float s) const { return Pixel(r_ * s, g_ * s, b_ * s, a_ * s); }
+	Pixel operator+(const Pixel &p) const { return Pixel(r_ + p.r_, g_ + p.g_, b_ + p.b_); }
 };
 
 Pixel &
@@ -187,17 +188,17 @@ public:
 	const Vector &d() const { return d_; }
 };
 
-class FuncFace {
-	static const float eps = 0.01f;
-
-public:
-	virtual ~FuncFace() {}
-
-	virtual float f(float x, float z, Vector *n) const = 0;
-	Vector dx(float x, float z) const { return Vector(2.0f * eps, f(x + eps, z, 0) - f(x - eps, z, 0), 0.0f); }
-	Vector dz(float x, float z) const { return Vector(0.0f, f(x, z + eps, 0) - f(x, z - eps, 0), 2.0f * eps); }
-	Vector n(float x, float z) const { return (dz(x, z) % dx(x, z)).norm(); }
-};
+float
+clamp(float x, float l, float h)
+{
+	if (x < l) {
+		return l;
+	} else if (x > h) {
+		return h;
+	} else {
+		return x;
+	}
+}
 
 template <class T>
 class Interpolator {
@@ -206,6 +207,20 @@ public:
 
 	virtual T f(float x) const = 0;
 	virtual T df(float x) const = 0;
+};
+
+template <class T>
+class LinearInterpolator : public Interpolator<T> {
+	const T p0_, p1_;
+
+public:
+	LinearInterpolator(const T &p0, const T &p1)
+	: p0_(p0)
+	, p1_(p1)
+	{}
+
+	virtual T f(float x) const { return p0_ * (1.0f - x) + p1_ * x; }
+	virtual T df(float x) const { 0.0f; }
 };
 
 template <class T>
@@ -296,29 +311,51 @@ public:
 	}
 };
 
-class MyFuncFace : public FuncFace {
-	static float noise(int x) { return 1.0f - ((x * (x * x * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0; }
-	static float noise2d(int x, int z) { return noise(z * 1024 + x); }
+class FuncFace {
+public:
+	virtual ~FuncFace() {}
+
+	virtual float f(float x, float z) const = 0;
+	virtual Pixel color(const Vector &k) const = 0;
+};
+
+class FuncFaceDerivator {
+	const FuncFace &f_;
+	const float eps_;
 
 public:
-//	virtual float f(float x, float z) const { return sinf(x) * sinf(z); }
-	float g(float x, float z, Vector *n) const;
-	float h(float x, float z, Vector *n) const;
-	virtual float f(float x, float z, Vector *n) const;
+	FuncFaceDerivator(const FuncFace &face, float eps = 0.01f)
+	: f_(face)
+	, eps_(eps)
+	{}
+
+	Vector dx(float x, float z) const { return Vector(2.0f * eps_, f_.f(x + eps_, z) - f_.f(x - eps_, z), 0.0f); }
+	Vector dz(float x, float z) const { return Vector(0.0f, f_.f(x, z + eps_) - f_.f(x, z - eps_), 2.0f * eps_); }
+	Vector n(float x, float z) const { return (dz(x, z) % dx(x, z)).norm(); }
+};
+
+class PerlinNoise {
+	static float noise1f(int x) { return 1.0f - ((x * (x * x * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0; }
+	static float noise2f(int x, int y) { return noise1f(y * 1024 + x); }
+	static float noise3f(int x, int y, int z) { return noise1f((z * 1024 + y) * 1024 + x); }
+
+public:
+	static float bicubic(float x, float z, Vector *n);
+	static float bilinear(float x, float z);
+	static float trilinear(float x, float y, float z);
 };
 
 float
-MyFuncFace::g(float x, float z, Vector *n)
-const
+PerlinNoise::bicubic(float x, float z, Vector *n)
 {
 	int ix = int(floor(x));
 	int iz = int(floor(z));
 	x -= ix, z -= iz;
 	float m[4][4] = {
-		{ noise2d(ix - 1, iz - 1), noise2d(ix, iz - 1), noise2d(ix + 1, iz - 1), noise2d(ix + 2, iz - 1) },
-		{ noise2d(ix - 1, iz), noise2d(ix, iz), noise2d(ix + 1, iz), noise2d(ix + 2, iz) },
-		{ noise2d(ix - 1, iz + 1), noise2d(ix, iz + 1), noise2d(ix + 1, iz + 1), noise2d(ix + 2, iz + 1) },
-		{ noise2d(ix - 1, iz + 2), noise2d(ix, iz + 2), noise2d(ix + 1, iz + 2), noise2d(ix + 2, iz + 2) }
+		{ noise2f(ix - 1, iz - 1), noise2f(ix, iz - 1), noise2f(ix + 1, iz - 1), noise2f(ix + 2, iz - 1) },
+		{ noise2f(ix - 1, iz), noise2f(ix, iz), noise2f(ix + 1, iz), noise2f(ix + 2, iz) },
+		{ noise2f(ix - 1, iz + 1), noise2f(ix, iz + 1), noise2f(ix + 1, iz + 1), noise2f(ix + 2, iz + 1) },
+		{ noise2f(ix - 1, iz + 2), noise2f(ix, iz + 2), noise2f(ix + 1, iz + 2), noise2f(ix + 2, iz + 2) }
 	};
 	float a = CubicInterpolator<float>(m[0][0], m[0][1], m[0][2], m[0][3]).f(x);
 	float b = CubicInterpolator<float>(m[1][0], m[1][1], m[1][2], m[1][3]).f(x);
@@ -337,93 +374,175 @@ const
 }
 
 float
-MyFuncFace::h(float x, float z, Vector *n)
-const
+PerlinNoise::bilinear(float x, float z)
 {
 	int ix = int(floor(x));
 	int iz = int(floor(z));
-	float ox = x, oz = z;
 	x -= ix, z -= iz;
 	float m[2][2] = {
-		{ noise2d(ix, iz), noise2d(ix + 1, iz) },
-		{ noise2d(ix, iz + 1), noise2d(ix + 1, iz + 1) }
+		{ noise2f(ix, iz), noise2f(ix + 1, iz) },
+		{ noise2f(ix, iz + 1), noise2f(ix + 1, iz + 1) }
 	};
 	float a = SmoothstepInterpolator<float>(m[0][0], m[0][1]).f(x);
 	float b = SmoothstepInterpolator<float>(m[1][0], m[1][1]).f(x);
 	SmoothstepInterpolator<float> ciz(a, b);
-	if (n) {
-/*		const float eps = 0.01f;
-		Vector dx(2.0f * eps, h(ox + eps, oz, 0) - h(ox - eps, oz, 0), 0.0f);
-		Vector dz(0.0f, h(ox, oz + eps, 0) - h(ox, oz - eps, 0), 2.0f * eps);
-		*n = Vector(dz % dx).norm();*/
-		float c = SmootherstepInterpolator<float>(m[0][0], m[1][0]).f(z);
-		float d = SmootherstepInterpolator<float>(m[0][1], m[1][1]).f(z);
-		SmootherstepInterpolator<float> cix(c, d);
-		*n = Vector(cix.df(x), 1.0f, ciz.df(z));
-	}
 	return ciz.f(z);
 }
 
 float
-MyFuncFace::f(float x, float z, Vector *n)
+PerlinNoise::trilinear(float x, float y, float z)
+{
+	int ix = int(floor(x));
+	int iy = int(floor(y));
+	int iz = int(floor(z));
+	x -= ix, y -= iy, z -= iz;
+	float a = SmoothstepInterpolator<float>(noise3f(ix, iy, iz), noise3f(ix + 1, iy, iz)).f(x);
+	float b = SmoothstepInterpolator<float>(noise3f(ix, iy + 1, iz), noise3f(ix + 1, iy + 1, iz)).f(x);
+	float c = SmoothstepInterpolator<float>(noise3f(ix, iy, iz + 1), noise3f(ix + 1, iy, iz + 1)).f(x);
+	float d = SmoothstepInterpolator<float>(noise3f(ix, iy + 1, iz + 1), noise3f(ix + 1, iy + 1, iz + 1)).f(x);
+	float e = SmoothstepInterpolator<float>(a, b).f(y);
+	float f = SmoothstepInterpolator<float>(c, d).f(y);
+	return SmoothstepInterpolator<float>(e, f).f(z);
+}
+
+class Ground : public FuncFace {
+public:
+	virtual float f(float x, float z) const;
+	virtual Pixel color(const Vector &k) const;
+};
+
+float
+Ground::f(float x, float z)
 const
 {
 	float y = 0.0f;
-	float a = 1.0f;
-	Vector no;
-	for (int i = 0; i < 6; ++i) {
-		float yo = a * h(x, z, n);
-//		if (n) yo /= n->dot(*n);
-		y += yo;
-		if (n) no = no + *n;
-		a *= 0.5f;
+	float a = 2.0f;
+	for (int i = 0; i < 8; ++i) {
+		float m = 1.0f / (1.0f + 2.0f * !i);
+		y += a * PerlinNoise::bilinear(x * m, z * m);
+		a *= 0.3f * m;
 		x *= 2.0f;
 		z *= 2.0f;
-	}
-	if (n) {
-		no.y(1.0f);
-		*n = no.norm();
 	}
 	return y;
 }
 
+Pixel
+Ground::color(const Vector &k)
+const
+{
+	return Pixel(1.0f, 0.5f, 0.0f) * k.dot(Vector(0.2f, 0.8f, 0.0f));
+}
+
+class Water : public FuncFace {
+public:
+	virtual float f(float x, float z) const;
+	virtual Pixel color(const Vector &k) const;
+};
+
+float
+Water::f(float x, float z)
+const
+{
+	float y = -1.7f;
+	float a = 0.01f;
+	x *= 10.0f, z *= 10.0f;
+	for (int i = 0; i < 3; ++i) {
+		y += a * PerlinNoise::bilinear(x, z);
+		a *= 0.5f;
+		x *= 2.0f;
+		z *= 2.0f;
+	}
+	return y;
+}
+
+Pixel
+Water::color(const Vector &k)
+const
+{
+	return Pixel(0.1f, 0.5f, 1.0f) * k.dot(Vector(0.1f, 0.6f, 0.3f));
+}
+
+class Clouds {
+public:
+	float f(float x, float y, float z) const;
+};
+
+float
+Clouds::f(float x, float y, float z)
+const
+{
+	float f = 0.0f;
+	float a = 1.0f;
+//	x *= 10.0f, y *= 10.0f, z *= 10.0f;
+	for (int i = 0; i < 3; ++i) {
+		f += a * PerlinNoise::trilinear(x, y, z);
+		a *= 0.5f;
+		x *= 2.0f;
+		y *= 2.0f;
+		z *= 2.0f;
+	}
+	return f;
+}
+
 class Scene {
-	const FuncFace &gnd_;
+	Ground gnd_;
+	Water wtr_;
+	Clouds cld_;
 	Vector light_;
 	Vector cam_;
 
 public:
-	Scene(FuncFace &gnd)
-	: gnd_(gnd)
-	{}
-
 	Scene &light(const Vector &light) { light_ = light; return *this; }
 	Scene &cam(const Vector &cam) { cam_ = cam; return *this; }
 
 	const Vector &cam() const { return cam_; }
 
-	Pixel cast(const Ray &r);
+	Pixel cast(const Ray &r) const;
 };
 
 Pixel
 Scene::cast(const Ray &r)
+const
 {
 	const float step = 0.02f;
 	float dt = step;
 	float ldt = dt;
 	const float tmin = 1.0f;
-	const float tmax = 10.0f;
-	float ld = 0.0f;
+	const float tmax = 50.0f;
+	float ldg = 0.0f;
+	float ldw = 0.0f;
+	float d = 0.0f;
 	for (float t = tmin; t < tmax; t += dt) {
+		dt = t * step;
 		Vector p(r.o() + r.d() * t);
-		if (p.y() > 2.0f && r.d().y() > 0.0f) break;
-		float h = gnd_.f(p.x(), p.z(), 0);
-		float cd = h - p.y();
-		if (cd > 0.0f) {
-			t -= ldt * cd / (ld + cd);
+		if (p.y() > 2.0f) {
+			d += cld_.f(p.x(), p.y(), p.z());
+			if (r.d().y() > 0.0f) continue;
+		}
+		float hg = gnd_.f(p.x(), p.z());
+		float hw = wtr_.f(p.x(), p.z());
+		const FuncFace *ff;
+		if (hw > hg) {
+			ff = &wtr_;
+		} else {
+			ff = &gnd_;
+		}
+		float cdg = hg - p.y();
+		float cdw = hw - p.y();
+		if (fmaxf(cdg, cdw) > 0.0f) {
+			if (cdg > cdw) {
+				t -= ldt * cdg / (ldg + cdg);
+			} else {
+				t -= ldt * cdw / (ldw + cdw);
+			}
 			p = r.o() + r.d() * t;
-			gnd_.f(p.x(), p.z(), 0);
-			Vector n(gnd_.n(p.x(), p.z()));
+			if (wtr_.f(p.x(), p.z()) > gnd_.f(p.x(), p.z())) {
+				ff = &wtr_;
+			} else {
+				ff = &gnd_;
+			}
+			Vector n(FuncFaceDerivator(*ff).n(p.x(), p.z()));
 			Vector l((light_ - p).norm());
 			Vector v((cam_ - p).norm());
 			Vector k(0.1f, l.dot(n));
@@ -433,17 +552,21 @@ Scene::cast(const Ray &r)
 				k.y(0.0f);
 			}
 //			return Pixel(h * h);
-			return Pixel(1.0f, 1.0f, 1.0f) * k.dot(Vector(0.1f, 0.6f, 0.3f));
-			if (n.x() < 0.0f) n.x(0.0f);
+			float f = fmaxf(t / tmax, 0.75f * (-1.2f - p.y()));
+//			float f = t / tmax;
+			return SmoothstepInterpolator<Pixel>(ff->color(k), Pixel(0.5f, 0.5f, 0.5f)).f(powf(clamp(f, 0.0f, 1.0f), 0.5f));
+/*			if (n.x() < 0.0f) n.x(0.0f);
 			if (n.y() < 0.0f) n.y(0.0f);
 			if (n.z() < 0.0f) n.z(0.0f);
-			return Pixel(n.x(), n.y(), n.z());
+			return Pixel(n.x(), n.y(), n.z());*/
 		}
 		ldt = dt;
-		ld = -cd;
-		dt = t * step;
+		ldg = -cdg;
+		ldw = -cdw;
 	}
-	return Pixel();
+	float blu = r.d().y() * r.d().y();
+	blu = 0.5f - 0.5f * blu;
+	return SmoothstepInterpolator<Pixel>(Pixel(blu, blu, 1.0f), Pixel(1.0f, 1.0f, 1.0f)).f(clamp(5.0f / tmax * d, 0.0f, 1.0f));
 }
 
 class Tracer {
@@ -465,7 +588,8 @@ Tracer::render(Pixmap &pm, Scene &s)
 		d.y(d.y() * pm.h() / pm.w());
 		for (int x = 0; x < pm.w(); ++x) {
 			d.x(2.0f * x / (pm.w() - 1) - 1.0f);
-			pm.at(x, y) = s.cast(Ray(s.cam(), d));
+			Ray r(s.cam(), d);
+			pm.at(x, y) = s.cast(r);
 		}
 		std::cout << y << std::endl;
 	}
@@ -475,11 +599,10 @@ Tracer::render(Pixmap &pm, Scene &s)
 int
 main()
 {
-	Pixmap pm(640, 480);
-	MyFuncFace ff1;
-	Scene s(ff1);
-	s.light(Vector(0.0f, 4.0f, 0.0f));
-	s.cam(Vector(0.0f, 1.0f, 0.0f));
-	Netbpm("x.ppm").save(Tracer(90.0f).render(pm, s));
+	Pixmap pm(1920, 1080);
+	Scene s;
+	s.light(Vector(10.0f, 4.0f, 10.0f));
+	s.cam(Vector(22.0f, 1.0f, 1.0f));
+	Netbpm("x.ppm").save(Tracer(120.0f).render(pm, s));
 	return 0;
 }
